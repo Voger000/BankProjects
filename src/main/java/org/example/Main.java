d1b3c650ea7083b0797e3eef43de61c0dc0993f6
“Roman <git.projects.umcs@gmail.com>
2024-03-04 14:07:01 +0100

    DataConection was be succsefuly conected

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,24 @@
 package org.example;
 import Bank.AccountUsersOfBank;
 import Bank.BalanceAccountOfUsers;
-
+import Bank.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Параметри підключення до бази даних MySQL
+        String url = "jdbc:mysql://localhost:3306/MySQL"; // URL для з'єднання з базою даних
+        String username = "root"; // Ім'я користувача для підключення
+        String password = "Qwertyuiop123QWE@"; // Пароль для підключення
 
+        // Створення об'єкту Connection для з'єднання з базою даних
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            if (conn != null) {
+                System.out.println("Підключення до бази даних MySQL успішне!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Помилка підключення до бази даних MySQL: " + ex.getMessage());
+        }
     }
 }
