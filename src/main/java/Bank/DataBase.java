eaafb2c037d6e12b4841b39813b2a1e333becec4
“Roman <git.projects.umcs@gmail.com>
2024-03-04 20:47:09 +0100

    Moved Connection from main to class DataBase

--- src/main/java/Bank/DataBase.java
+++ src/main/java/Bank/DataBase.java
@@ -1,12 +1,22 @@
 package Bank;
-
+import Bank.AccountUsersOfBank;
+import Bank.BalanceAccountOfUsers;
+import Bank.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DataBase {
-    public static void main(String[] args) {
 
-
+    public static void ConnectionToDatabase(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/mysql";
+        String username = "root";
+        String password = "Qwertyuiop123QWE@";
+        try (Connection conn = DriverManager.getConnection(url, username, password)) {
+            if (conn != null) {
+                System.out.println("Підключення до бази даних MySQL успішне!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Помилка підключення до бази даних MySQL: " + ex.getMessage());
+        }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,19 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Main {
-    public static void main(String[] args) {
-        // Параметри підключення до бази даних MySQL
-        String url = "jdbc:mysql://localhost:3306/MySQL"; // URL для з'єднання з базою даних
-        String username = "root"; // Ім'я користувача для підключення
-        String password = "Qwertyuiop123QWE@"; // Пароль для підключення
 
-        // Створення об'єкту Connection для з'єднання з базою даних
-        try (Connection conn = DriverManager.getConnection(url, username, password)) {
-            if (conn != null) {
-                System.out.println("Підключення до бази даних MySQL успішне!");
-            }
-        } catch (SQLException ex) {
-            System.out.println("Помилка підключення до бази даних MySQL: " + ex.getMessage());
-        }
+    public static void main(String[] args) {
+        DataBase dataBase = new DataBase();
+        dataBase.ConnectionToDatabase(args);
     }
 }
